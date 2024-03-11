70bea53eb990175162d72f19ae5f1c7460d5baf1
Joaovrt <joaovictorosa.tagliarini@gmail.com>
2024-03-11 16:58:16 -0300

    Soma

---
+++ calculadora/src/main/java/com/calculadora/calculadora/MathController.java
@@ -0,0 +1,33 @@
+package com.calculadora.calculadora;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value="/sum/{numOne}/{numTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable(value = "numOne") String numOne, @PathVariable(value = "numTwo") String numTwo )
+    throws Exception{
+        if(!isNumeric(numOne)||!isNumeric(numTwo)){
+            throw new Exception();
+        }
+        return convertToDouble(numOne)+convertToDouble(numTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+       if(strNumber==null) return 0D;
+       String number  = strNumber.replaceAll(",",".");
+       if (isNumeric(number)) return Double.parseDouble(number);
+       return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if(strNumber==null) return false;
+        String number  = strNumber.replaceAll(",",".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
